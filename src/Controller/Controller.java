7d9a26fda0d99bca04dff255356118763184898d
Tom Hester <tomhester113@gmail.com>
2020-08-29 13:39:06 -0600

    Ref (#3)
    * C482 UI
    
    * C482 UI

---
+++ src/Controller/Controller.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+  @FXML
+  public void onActionExit(ActionEvent event) {
+
+    System.exit(0);
+
+  }
+
+}
